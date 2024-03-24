4e4895d8d2f8bc1b670704d395cf1b91d3041df4
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2024-03-24 12:48:22 +0700

    stage4

--- lottery.sol
+++ lottery.sol
@@ -16,6 +16,7 @@ contract Lottery is CommitReveal {
 		bytes32 NumHash;
 		uint Num;
 		uint16 order;
+		bool canWithdraw;
 	}
 
 	mapping(address => Player) public player;
@@ -39,6 +40,7 @@ contract Lottery is CommitReveal {
 		numOfUser += 1;
 		player[msg.sender].order = numOfUser;
 		playerAddress[numOfUser] = msg.sender;
+		player[msg.sender].canWithdraw = true;
 	}
 
 	function reveal(uint _randNum, uint _salt) public {
@@ -47,12 +49,12 @@ contract Lottery is CommitReveal {
 		player[msg.sender].Num = _randNum;
 	}
 
-	function Determine_the_winner() private {
+	function Determine_the_winner() public {
 		require(block.timestamp < startTime + T1 + T2 + T3 && block.timestamp > startTime + T1 + T2, "stage 3 is over/not time yet.");
-		require(msg.sender == owner);
+		require(msg.sender == owner, "are you owner?");
 		uint winnerNum = 0;
 		uint16 newN = 0;
-		for (uint i=1; i <= N; i++) {
+		for (uint i=1; i <= numOfUser; i++) {
 			if(!commits[playerAddress[i]].revealed
 			|| (player[playerAddress[i]].Num > 999 || player[playerAddress[i]].Num < 0)) {
 				continue; 
@@ -61,21 +63,30 @@ contract Lottery is CommitReveal {
 			winnerNum ^= player[playerAddress[i]].Num;
 			player[playerAddress[i]].order = newN;
 			playerAddress[newN] = playerAddress[i];
+			player[msg.sender].canWithdraw == false;
 		}
-		N = newN;
-		if(N == 0) {
-			owner.transfer(0.001 ether * N);
+		if(newN == 0) {
+			owner.transfer(0.001 ether * newN);
 		} else {
-			winnerNum %= N;
+			winnerNum %= newN;
 			address payable winner = payable(playerAddress[winnerNum]);
-            winner.transfer(0.00098 ether * N);
-            owner.transfer(0.0002 ether * N);
+            winner.transfer(0.00098 ether * newN);
+            owner.transfer(0.00002 ether * newN);
 		}
 	}
 
 	function withdraw() public {
 		require(block.timestamp > startTime + T1 + T2 + T3, "stage 4 is not time yet.");
+		require(player[msg.sender].canWithdraw == true, "canWithdraw == false");
+		player[msg.sender].canWithdraw == false;
 		address payable payback = payable(msg.sender);
 		payback.transfer(0.001 ether);
+
+	}
+
+	function restart() public {
+		require(address(this).balance == 0);
+		require(msg.sender == owner);
+		numOfUser = 0;
 	}
 }
