0a48a817fe04a60b5c6335f0f44af21a6d28b2e9
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2024-03-24 11:14:39 +0700

    stage3

--- lottery.sol
+++ lottery.sol
@@ -7,25 +7,25 @@ contract Lottery is CommitReveal {
 	uint public startTime;
     uint public T1;
 	uint public T2;
+	uint public T3;
     uint public N; 
 	uint public constant TICKET_PRICE = 0.001 ether;
 	uint16 public numOfUser = 0;
-
-	struct User {
+	address payable owner = payable(msg.sender);
+	struct Player {
 		bytes32 NumHash;
 		uint Num;
 		uint16 order;
 	}
 
-	mapping(address => User) public user;
-
-    constructor(uint _N, uint _T1, uint _T2) {
-        T1 = _T1 * 1 minutes; //Convert to minutes
-        T2 = _T2 * 1 minutes;
+	mapping(address => Player) public player;
+	mapping(uint => address) public playerAddress;
+    constructor(uint _N, uint _T1, uint _T2, uint _T3) {
+        T1 = _T1;
+        T2 = _T2;
+		T3 = _T3;
 		N = _N;
     }
-
-	
 
     function Pick_a_number(uint _randNum, uint _salt) public payable {
 		if(numOfUser == 0) {
@@ -33,18 +33,44 @@ contract Lottery is CommitReveal {
 		}
         require(block.timestamp < startTime + T1, "stage 1 is over.");
         require(msg.value == TICKET_PRICE, "Incorrect TICKET_PRICE.");
-		require(_randNum >= 0 && _randNum <= 999);
 		require(numOfUser < N, "Tickets are sold out.");
-
-		user[msg.sender].NumHash = getSaltedHash(bytes32(_randNum), bytes32(_randNum + _salt));
-		commit(user[msg.sender].NumHash);
+		player[msg.sender].NumHash = getSaltedHash(bytes32(_randNum), bytes32(_randNum + _salt));
+		commit(player[msg.sender].NumHash);
 		numOfUser += 1;
-		user[msg.sender].order = numOfUser;
+		player[msg.sender].order = numOfUser;
+		playerAddress[numOfUser] = msg.sender;
 	}
 
 	function reveal(uint _randNum, uint _salt) public {
 		require(block.timestamp < startTime + T1 + T2 && block.timestamp > startTime + T1, "stage 2 is over/not time yet.");
 		revealAnswer(bytes32(_randNum), bytes32(_randNum + _salt));
-		user[msg.sender].Num = _randNum;
+		player[msg.sender].Num = _randNum;
 	}
+
+	function Determine_the_winner() private {
+		require(block.timestamp < startTime + T1 + T2 + T3 && block.timestamp > startTime + T1 + T2, "stage 3 is over/not time yet.");
+		require(msg.sender == owner);
+		uint winnerNum = 0;
+		uint16 newN = 0;
+		for (uint i=1; i <= N; i++) {
+			if(!commits[playerAddress[i]].revealed
+			|| (player[playerAddress[i]].Num > 999 || player[playerAddress[i]].Num < 0)) {
+				continue; 
+			}
+			newN++;
+			winnerNum ^= player[playerAddress[i]].Num;
+			player[playerAddress[i]].order = newN;
+			playerAddress[newN] = playerAddress[i];
+		}
+		N = newN;
+		if(N == 0) {
+			owner.transfer(0.001 ether * N);
+		} else {
+			winnerNum %= N;
+			address payable winner = payable(playerAddress[winnerNum]);
+            winner.transfer(0.00098 ether * N);
+            owner.transfer(0.0002 ether * N);
+		}
+	}
+
 }
